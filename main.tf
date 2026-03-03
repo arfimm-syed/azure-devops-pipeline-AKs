a0deb937eb8c839e431edbfbe930e783f291db72
arfimm-syed <arfimm@gmail.com>
2026-03-03 23:19:39 +0000

    update main

--- main.tf
+++ main.tf
@@ -8,19 +8,19 @@ resource "azurerm_resource_group" "rg" {
   location = "UK South"
 }
 
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "aks-dev-cluster"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "aksdns"
+#resource "azurerm_kubernetes_cluster" "aks" {
+ # name                = "aks-dev-cluster"
+ # location            = azurerm_resource_group.rg.location
+ # resource_group_name = azurerm_resource_group.rg.name
+ # dns_prefix          = "aksdns"
 
-  default_node_pool {
-    name       = "agentpool"
-    node_count = 1
-    vm_size    = "Standard_B2ms"
-  }
+ # default_node_pool {
+ #   name       = "agentpool"
+ #   node_count = 1
+  #  vm_size    = "Standard_B2ms"
+  #}
 
-  identity {
-    type = "SystemAssigned"
-  }
-}
+ # identity {
+  #  type = "SystemAssigned"
+  #}
+#}
