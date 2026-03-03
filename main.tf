51f3f74635b382c9f4a0f0f355f8d141f173255f
Afzal <arfimm@gmail.com>
2026-03-03 23:46:13 +0000

    update storage

--- main.tf
+++ main.tf
@@ -8,6 +8,19 @@ resource "azurerm_resource_group" "rg" {
   location = "UK South"
 }
 
+
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "stosdsrtname"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
 #resource "azurerm_kubernetes_cluster" "aks" {
  # name                = "aks-dev-cluster"
  # location            = azurerm_resource_group.rg.location
